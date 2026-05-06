05b7dde7e206a22c1e8f9fddf13320586907bea6
Ekaterina <varfolomeeva_ea@mail.ru>
2026-05-06 21:16:26 +0000

    Add init.sql for database setup

---
+++ sql/init.sql
@@ -0,0 +1,14 @@
+-- Logs
+\i sql/logs/ddl/001_create_schema.sql
+\i sql/logs/ddl/002_create_tables.sql
+
+-- DS
+\i sql/ds/ddl/001_create_schema.sql
+\i sql/ds/ddl/002_create_tables.sql
+\i sql/ds/procedures/fill_account_turnover_f.sql
+\i sql/ds/procedures/fill_account_balance_f.sql
+
+-- DM
+\i sql/dm/ddl/001_create_schema.sql
+\i sql/dm/ddl/002_create_tables.sql
+\i sql/dm/procedures/fill_f101_round_f.sql
